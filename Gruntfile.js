41cb96c3ef12bd9aa548511f6af7293ea85cfa8c
Dima Fitiskin <dfitiskin@gmail.com>
2015-12-20 00:58:35 +0400

    Установлен и настроен Grunt FTP push

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,30 @@ module.exports = function(grunt) {
 					'css/main.css': 'styles/main.less'
 				}
 			}
-		}
+		},
+		ftp_push: {
+		    demo: {
+		    	options: {
+		    		authKey: 'netology',
+		    		host: 'university.netology.ru',
+		    		dest: '/fbb-store/',
+		    		port: 21
+		    	},
+		    	files: [{
+		    		expand: true,
+		    		cwd: '.',
+		    		src: [
+		    		      'index.html',
+		    		      'css/main.css'
+		    		]
+		        }]
+		    }
+		 }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-ftp-push');
 	
-	grunt.registerTask('default', [ 'uglify' ]);
+	grunt.registerTask('default', ['less', 'ftp_push']);
 
 };
